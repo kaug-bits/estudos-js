4e52acbe3fe4f6bd2f70dbd1e48f0714171d8869
kaug-bits <jogandokauanaugusto@gmail.com>
2026-02-25 23:47:44 -0300

    terminando aula 3

--- Aula2/script.js
+++ Aula2/script.js
@@ -1,24 +1,27 @@
 function adicionarTarefa() {
 
     //recebe valor do input do usuário
-    let inputTarefa = document.getElementById("inputTarefa")
-    let tarefa = inputTarefa.value
+    const inputTarefa = document.getElementById("inputTarefa")
+    let tarefa = inputTarefa.value.trim()
 
-    //cria novo item (li) e insere na (lista ul)
-    let listaTarefas = document.getElementById("listaTarefas")
-    let novaTarefa = document.createElement("li")
-    novaTarefa.textContent = tarefa
-    listaTarefas.appendChild(novaTarefa)
+    const mensagem = document.getElementById("mensagem")
 
-    //mensagem de tarefa adicionada com sucesso
-    let mensagem = "Tarefa adicionada com sucesso!"
-    document.getElementById("mensagem").textContent = mensagem
+    if (tarefa == ""){
+        let mensagemErro = "Digite uma tarefa para adiciona-la na sua lista"
+        mensagem.textContent = mensagemErro
+    }else{
+        //mensagem de tarefa adicionada com sucesso
+        let mensagemSucesso = "Tarefa adicionada com sucesso!"
+        mensagem.textContent = mensagemSucesso
+
+        //cria novo item (li) e insere na (lista ul)
+        const listaTarefas = document.getElementById("listaTarefas")
+        let novaTarefa = document.createElement("li")
+        novaTarefa.textContent = tarefa
+        listaTarefas.appendChild(novaTarefa)
+    }   
 
     //limpa o input do usuário
     inputTarefa.value = ""
 
-    if (tarefa == ""){
-        let mensagemErro = "Digite uma tarefa para adiciona-la na sua lista"
-        document.getElementById("mensagem")
-    }
 }
