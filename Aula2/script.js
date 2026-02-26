4394d2b9af7239c005022544fb642f1763e979d0
kaug-bits <jogandokauanaugusto@gmail.com>
2026-02-25 23:18:20 -0300

    Primeiro commit - estudos de JavaScript

---
+++ Aula2/script.js
@@ -0,0 +1,24 @@
+function adicionarTarefa() {
+
+    //recebe valor do input do usuário
+    let inputTarefa = document.getElementById("inputTarefa")
+    let tarefa = inputTarefa.value
+
+    //cria novo item (li) e insere na (lista ul)
+    let listaTarefas = document.getElementById("listaTarefas")
+    let novaTarefa = document.createElement("li")
+    novaTarefa.textContent = tarefa
+    listaTarefas.appendChild(novaTarefa)
+
+    //mensagem de tarefa adicionada com sucesso
+    let mensagem = "Tarefa adicionada com sucesso!"
+    document.getElementById("mensagem").textContent = mensagem
+
+    //limpa o input do usuário
+    inputTarefa.value = ""
+
+    if (tarefa == ""){
+        let mensagemErro = "Digite uma tarefa para adiciona-la na sua lista"
+        document.getElementById("mensagem")
+    }
+}
