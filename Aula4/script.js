239580405144383ab2973f4620c27668c4253d1e
kaug-bits <jogandokauanaugusto@gmail.com>
2026-03-01 00:21:06 -0300

    finalizando aula 4-arrays,for

--- Aula4/script.js
+++ Aula4/script.js
@@ -1,6 +1,9 @@
+let tarefas = []
 function adicionarTarefa() {
 
-    //recebe valor do input do usuário
+    
+
+
     const inputTarefa = document.getElementById("inputTarefa")
     let tarefa = inputTarefa.value.trim()
 
@@ -10,18 +13,32 @@ function adicionarTarefa() {
         let mensagemErro = "Digite uma tarefa para adiciona-la na sua lista"
         mensagem.textContent = mensagemErro
     }else{
-        //mensagem de tarefa adicionada com sucesso
+        
         let mensagemSucesso = "Tarefa adicionada com sucesso!"
         mensagem.textContent = mensagemSucesso
 
-        //cria novo item (li) e insere na (lista ul)
-        const listaTarefas = document.getElementById("listaTarefas")
-        let novaTarefa = document.createElement("li")
-        novaTarefa.textContent = tarefa
-        listaTarefas.appendChild(novaTarefa)
-    }   
+        tarefas.push(tarefa)
+
+        redenrizarTarefas()
+    }
+    
+    
 
     //limpa o input do usuário
     inputTarefa.value = ""
 
+}
+
+function redenrizarTarefas(){
+        const listaTarefas = document.getElementById("listaTarefas")
+        listaTarefas.innerHTML = ""
+        let i = 0
+        for(i; i< tarefas.length; i++)
+        {
+            let novaTarefa = document.createElement("li")
+            novaTarefa.textContent = tarefas[i]
+            listaTarefas.appendChild(novaTarefa)
+        }
+
+        
 }
